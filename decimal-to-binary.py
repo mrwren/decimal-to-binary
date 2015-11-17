023854433f16d8918be88897918e903d49952bcd
mrwren <mrwren@users.noreply.github.com>
2015-11-17 12:33:43 +0200

    code

---
+++ decimal-to-binary.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from math import log
+
+array = []
+n = int(sys.argv[1])
+pows = int(log(n, 2))
+x = range(pows + 1)
+x.reverse()
+for i in x:
+	if pow(2, i) > n:
+		array.append(str(0))
+	else:
+		n = n - pow(2, i)
+		array.append(str(1))
+print ''.join(array)
